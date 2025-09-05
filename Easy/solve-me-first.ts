577a2b6ba655e5152712bfec7bb5525ab64e7444
Sidenir Junior <sidenirdev@gmail.com>
2025-09-05 08:39:29 -0300

    Easy: solve-me-first

---
+++ Easy/solve-me-first.ts
@@ -0,0 +1,9 @@
+function main() {
+    const a: number = parseInt(readLine())
+    const b: number = parseInt(readLine())
+    
+    const result = a + b;
+    
+    console.log(result)
+}
+//Link to the problem: https://www.hackerrank.com/challenges/solve-me-first/problem

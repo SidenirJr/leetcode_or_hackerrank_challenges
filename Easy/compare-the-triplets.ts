ce80f77886652f39cc1a745fee59ff3e034a0b2d
Sidenir Junior <sidenirdev@gmail.com>
2025-09-05 08:38:23 -0300

    Easy: compare the triplets

---
+++ Easy/compare-the-triplets.ts
@@ -0,0 +1,25 @@
+//Link to the problem: https://www.hackerrank.com/challenges/compare-the-triplets/problem
+
+/*
+ * Complete the 'compareTriplets' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+function compareTriplets(a: number[], b: number[]): number[] {
+    let bob: number = 0;
+    let alice: number = 0;
+    
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] > b[i]) {
+            alice++;
+        } else if (a[i] < b[i]) {
+            bob++;
+        }
+    }
+    
+    return [alice, bob]
+}

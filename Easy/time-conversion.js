2eb27052ea88dc99d480c079233c8f0bae598fe0
Sidenir Junior <sidenirdev@gmail.com>
2025-09-16 13:33:30 -0300

    Easy: time conversion

---
+++ Easy/time-conversion.js
@@ -0,0 +1,24 @@
+//Link: https://www.hackerrank.com/challenges/time-conversion/problem
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function timeConversion(s : string): string {
+    let periodo = s.slice(-2);
+    let [hora, minuto, segundo] = s.slice(0, -2).split(":");
+
+    hora = parseInt(hora, 10);
+
+    if (periodo === "AM") {
+        if (hora === 12) hora = 0;
+    } else {
+        if (hora !== 12) hora += 12;
+    }
+
+    let hora24h = String(hora).padStart(2, "0");
+
+    return `${hora24h}:${minuto}:${segundo}`;
+}
